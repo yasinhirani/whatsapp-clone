6eec6f24e1fe1ccf307fe883af170fa5e6331b93
Yasin Hirani <yasin.hirani@1rivet.local>
2021-11-08 13:38:08 +0530

    Fix: search functionality

--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,6 +1,92 @@
 import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
+const OriginalChatList = [{
+  id: 1,
+  name: 'Abc',
+  time: '12:30',
+  message: 'This is dummy text'
+},
+{
+  id: 2,
+  name: 'Def',
+  time: '12:30',
+  message: 'This is dummy text'
+},
+{
+  id: 3,
+  name: 'Ghi',
+  time: '12:30',
+  message: 'This is dummy text'
+},
+{
+  id: 4,
+  name: 'Jkl',
+  time: '12:30',
+  message: 'This is dummy text'
+},
+{
+  id: 5,
+  name: 'Mno',
+  time: '12:30',
+  message: 'This is dummy text'
+},
+{
+  id: 6,
+  name: 'Pqr',
+  time: '12:30',
+  message: 'This is dummy text'
+},
+{
+  id: 7,
+  name: 'Stu',
+  time: '12:30',
+  message: 'This is dummy text'
+},
+{
+  id: 8,
+  name: 'Vwx',
+  time: '12:30',
+  message: 'This is dummy text'
+},
+{
+  id: 9,
+  name: 'Yzz',
+  time: '12:30',
+  message: 'This is dummy text'
+},
+{
+  id: 10,
+  name: 'Jkl',
+  time: '12:30',
+  message: 'This is dummy text'
+},
+{
+  id: 11,
+  name: 'Jkl',
+  time: '12:30',
+  message: 'This is dummy text'
+},
+{
+  id: 12,
+  name: 'Jkl',
+  time: '12:30',
+  message: 'This is dummy text'
+},
+{
+  id: 13,
+  name: 'Jkl',
+  time: '12:30',
+  message: 'This is dummy text'
+},
+{
+  id: 14,
+  name: 'Jkl',
+  time: '12:30',
+  message: 'This is dummy text'
+},
+];
+
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
@@ -8,91 +94,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
-  chats_list=[{
-    id: 1,
-    name: 'Abc',
-    time: '12:30',
-    message: 'This is dummy text'
-  },
-  {
-    id: 2,
-    name: 'Def',
-    time: '12:30',
-    message: 'This is dummy text'
-  },
-  {
-    id: 3,
-    name: 'Ghi',
-    time: '12:30',
-    message: 'This is dummy text'
-  },
-  {
-    id: 4,
-    name: 'Jkl',
-    time: '12:30',
-    message: 'This is dummy text'
-  },
-  {
-    id: 5,
-    name: 'Mno',
-    time: '12:30',
-    message: 'This is dummy text'
-  },
-  {
-    id: 6,
-    name: 'Pqr',
-    time: '12:30',
-    message: 'This is dummy text'
-  },
-  {
-    id: 7,
-    name: 'Stu',
-    time: '12:30',
-    message: 'This is dummy text'
-  },
-  {
-    id: 8,
-    name: 'Vwx',
-    time: '12:30',
-    message: 'This is dummy text'
-  },
-  {
-    id: 9,
-    name: 'Yzz',
-    time: '12:30',
-    message: 'This is dummy text'
-  },
-  {
-    id: 10,
-    name: 'Jkl',
-    time: '12:30',
-    message: 'This is dummy text'
-  },
-  {
-    id: 11,
-    name: 'Jkl',
-    time: '12:30',
-    message: 'This is dummy text'
-  },
-  {
-    id: 12,
-    name: 'Jkl',
-    time: '12:30',
-    message: 'This is dummy text'
-  },
-  {
-    id: 13,
-    name: 'Jkl',
-    time: '12:30',
-    message: 'This is dummy text'
-  },
-  {
-    id: 14,
-    name: 'Jkl',
-    time: '12:30',
-    message: 'This is dummy text'
-  },
-];
+  chats_list= OriginalChatList;
 
 @Output() getName:EventEmitter<any> = new EventEmitter();
 
@@ -108,8 +110,7 @@ getClicked(name: any){
 
   search(){
     if (this.firstName == "") {
-      console.log("enter name");
-      this.ngOnInit();
+      this.chats_list = OriginalChatList;
       
     }
     else{
